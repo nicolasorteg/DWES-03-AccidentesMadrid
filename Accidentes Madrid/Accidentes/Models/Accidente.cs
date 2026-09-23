0474ec94780490fb663f3826fcd6a58f0f712048
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-23 14:36:25 +0200

    add accidente model

---
+++ Accidentes Madrid/Accidentes/Models/Accidente.cs
@@ -0,0 +1,39 @@
+﻿using Accidentes.Enums;
+
+namespace Accidentes.Models;
+
+/// <summary>
+/// Record que representa un Accidente
+/// </summary>
+public record Accidente {
+    
+    public string NumExpediente { get; init; } = string.Empty;
+
+    public DateOnly Fecha { get; init; }
+    public TimeOnly Hora { get; init; }
+
+    public string Localizacion { get; init; } = string.Empty;
+    public string? NumCalle { get; init; } // nullable
+    public int? CodDistrito { get; init; }
+    public string Distrito { get; init; } = string.Empty;
+
+    public string TipoAccidente { get; init; } = string.Empty;
+
+    public string EstadoMeteorologico { get; init; } = string.Empty;
+
+    public string TipoVehiculo { get; init; } = string.Empty;
+    
+    public string TipoPersona { get; init; } = string.Empty;
+    public string RangoEdad { get; init; } = string.Empty;
+    public Sexo Sexo { get; init; }
+
+    public TipoAccidente CodigoAccidente { get; init; }
+
+    public Lesividad CodLesividad { get; init; }
+
+    public double CoordenadaXUtm { get; init; }
+    public double CoordenadaYUtm { get; init; }
+
+    public bool PositivaAlcohol { get; init; }
+    public bool PositivaDroga { get; init; }
+}
