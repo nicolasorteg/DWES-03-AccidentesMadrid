c60cce417c52fc3e98ef32b4f4dd118202f3d1bd
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-23 20:47:38 +0200

    init mapper

---
+++ Accidentes Madrid/Accidentes/Mapper/AccidenteMapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using Accidentes.Models;
+using CsvHelper.Configuration;
+
+namespace Accidentes.Mapper;
+
+public sealed class AccidenteMapper : ClassMap<Accidente> {
+    
+    private static readonly CultureInfo Es = CultureInfo.GetCultureInfo("es-ES"); // para separador decimal con coma
+    private static readonly string[] FormatosHora = ["H:mm:ss", "HH:mm:ss"];
+
+    public AccidenteMapper() {
+        
+    }
+    
+}
